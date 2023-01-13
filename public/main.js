611c6fa8f28682da96f88f2c6e542651d75cc1f7
Julia Moore <peony.the.pretty@gmail.com>
2023-01-12 18:12:58 -0600

    rollbar added

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => {
+    
+    alert('The cat name is: Rascal')
+}
 
 btn.addEventListener('click', clickHandler)
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const path = require(`path`)
 const app = express()
+require(`dotenv`).config()
+const {ROLLBAR_TOKEN} = process.env
 
-app.use(express.static(`${__dirname}/public`))
+// app.use(express.static(`${__dirname}/public`))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'))
-// })
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+rollbar.log(`Hello World!`)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.log(`File requested`)
+})
+
 
 app.listen(4000, () => {
     console.log(`server running on port 4000`)
